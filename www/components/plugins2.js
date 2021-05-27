8c11d17aec3a755865e8fe5bdbfdb3f86cacff38
Felipe1501 <felipebsantos1501@gmail.com>
2021-05-28 06:29:58 +0900

    errin na hora da img aparecer na tela

--- www/components/plugins2.js
+++ www/components/plugins2.js
@@ -29,17 +29,17 @@ $(document).on("click", "#qrcode", function(){
           alert("fALHA AO SCANEAR: " + error);
       },
       {
-          preferFrontCamera : false, // iOS and Android
-          showFlipCameraButton : true, // iOS and Android
-          showTorchButton : true, // iOS and Android
-          torchOn: true, // Android, launch with the torch switched on (if available)
-          saveHistory: true, // Android, save scan history (default false)
-          prompt : "Coloque a Camera no Código para Scanear", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS and Android
+          preferFrontCamera : false,
+          showFlipCameraButton : true, 
+          showTorchButton : true, 
+          torchOn: true, 
+          saveHistory: true,
+          prompt : "Coloque a Camera no Código para Scanear", 
+          resultDisplayDuration: 500, 
+          formats : "QR_CODE,PDF_417,CODE_39",
+          orientation : "landscape", 
+          disableAnimations : true, 
+          disableSuccessBeep: false 
       }
    );
 
@@ -49,16 +49,16 @@ $(document).on("click", "#qrcode", function(){
     var networkState = navigator.connection.type;
 
     var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
+    states[Connection.UNKNOWN]  = 'Conexão Desconhecida';
+    states[Connection.ETHERNET] = 'Ethernet Conexão';
+    states[Connection.WIFI]     = 'Conexão WiFi';
+    states[Connection.CELL_2G]  = 'Cell 2G Conexão';
+    states[Connection.CELL_3G]  = 'Cell 3G Conexão';
+    states[Connection.CELL_4G]  = 'Cell 4G Conexão';
+    states[Connection.CELL]     = 'Cell Genérico conexãon';
+    states[Connection.NONE]     = 'Não Conexão Internet';
 
-    alert('Connection type: ' + states[networkState]);
+    alert('Tipo de Conexão: ' + states[networkState]);
 }
 
 $(document).on("click", "#conexao", function(){
